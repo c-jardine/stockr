7322b70c5a3beb32655d71a313f4679d0d316dab
Chris Jardine <chris@keplux.com>
2024-08-28 18:30:53 -0400

    Add controlled select component.

---
+++ src/components/ControlledSelect/ControlledSelect.tsx
@@ -0,0 +1,79 @@
+import { FormControl, FormErrorMessage, FormLabel } from "@chakra-ui/react";
+import {
+  Select,
+  type GroupBase,
+  type CreatableProps as SelectProps,
+} from "chakra-react-select";
+import React from "react";
+import {
+  useController,
+  type FieldValues,
+  type UseControllerProps,
+} from "react-hook-form";
+
+export type MultiSelectInput = {
+  label: string;
+  value: string;
+};
+
+interface ControlledSelectProps<
+  FormValues extends FieldValues = FieldValues,
+  Option = unknown,
+  IsMulti extends boolean = boolean,
+  Group extends GroupBase<Option> = GroupBase<Option>
+> extends Omit<SelectProps<Option, IsMulti, Group>, "name" | "defaultValue">,
+    UseControllerProps<FormValues> {
+  label?: string | React.ReactNode;
+}
+
+/**
+ * An attempt to make a reusable chakra-react-select form component
+ *
+ * @param props - The combined props of the chakra-react-select component and the useController hook
+ */
+export function ControlledSelect<
+  FormValues extends FieldValues = FieldValues,
+  Option = unknown,
+  IsMulti extends boolean = boolean,
+  Group extends GroupBase<Option> = GroupBase<Option>
+>({
+  name,
+  label,
+  options,
+  control,
+  rules,
+  shouldUnregister,
+  ...selectProps
+}: ControlledSelectProps<FormValues, Option, IsMulti, Group>) {
+  const {
+    field,
+    fieldState: { error },
+  } = useController<FormValues>({
+    name,
+    control,
+    rules,
+    shouldUnregister,
+  });
+
+  return (
+    <FormControl id={name} isInvalid={!!error}>
+      {label && <FormLabel w="full">{label}</FormLabel>}
+      <Select<Option, IsMulti, Group>
+        options={options}
+        chakraStyles={{
+          option: (provided) => ({
+            ...provided,
+            fontSize: "sm",
+          }),
+          multiValueLabel: (provided) => ({
+            ...provided,
+            fontSize: "xs",
+          }),
+        }}
+        {...selectProps}
+        {...field}
+      />
+      {error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+    </FormControl>
+  );
+}
