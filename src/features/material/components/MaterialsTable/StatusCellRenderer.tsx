6525782528c0e3cd2439ae402808d1285c171b76
Chris Jardine <chris@keplux.com>
2024-08-29 02:11:19 -0400

    Remove improper use of hooks.

--- src/features/material/components/MaterialsTable/StatusCellRenderer.tsx
+++ src/features/material/components/MaterialsTable/StatusCellRenderer.tsx
@@ -16,41 +16,42 @@ import { type MaterialsTableRows } from "./MaterialsTable";
 export function StatusCellRenderer({
   node,
 }: CustomCellRendererProps<MaterialsTableRows>) {
+  const greenStyles = {
+    border: "1px",
+    borderColor: useColorModeValue("green.300", "green.900"),
+    bg: useColorModeValue("green.100", "green.950"),
+    color: useColorModeValue("green.800", "green.500"),
+  };
+
+  const yellowStyles = {
+    border: "1px",
+    borderColor: useColorModeValue("yellow.300", "yellow.900"),
+    bg: useColorModeValue("yellow.100", "yellow.950"),
+    color: useColorModeValue("yellow.800", "yellow.500"),
+  };
+
+  const redStyles = {
+    border: "1px",
+    borderColor: useColorModeValue("red.300", "red.900"),
+    bg: useColorModeValue("red.100", "red.950"),
+    color: useColorModeValue("red.800", "red.500"),
+  };
+
   function getStyles(status: QuantityStatus): TagProps {
     switch (status) {
-      case "Available": {
-        // TODO: Fix this
-        return {
-          border: "1px",
-          borderColor: useColorModeValue("green.300", "green.900"),
-          bg: useColorModeValue("green.100", "green.950"),
-          color: useColorModeValue("green.800", "green.500"),
-        };
-      }
-      case "Low stock": {
-        return {
-          border: "1px",
-          borderColor: useColorModeValue("yellow.300", "yellow.900"),
-          bg: useColorModeValue("yellow.100", "yellow.950"),
-          color: useColorModeValue("yellow.800", "yellow.500"),
-        };
-      }
-      case "Out of stock": {
-        return {
-          border: "1px",
-          borderColor: useColorModeValue("red.300", "red.900"),
-          bg: useColorModeValue("red.100", "red.950"),
-          color: useColorModeValue("red.800", "red.500"),
-        };
-      }
-      default: {
+      case "Available":
+        return greenStyles;
+      case "Low stock":
+        return yellowStyles;
+      case "Out of stock":
+        return redStyles;
+      default:
         return {};
-      }
     }
   }
 
   if (!node.data?.status) {
-    return null;
+    return "—";
   }
 
   return (
