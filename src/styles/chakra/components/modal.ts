426ce3f7518a6175fb7be7d3fa9f97d07602474d
Chris Jardine <chris@keplux.com>
2024-08-30 15:23:36 -0400

    Remove unused code.

--- src/styles/chakra/components/modal.ts
+++ src/styles/chakra/components/modal.ts
@@ -9,7 +9,7 @@ const baseStyle = helper.definePartsStyle((props) => ({
     bg: mode("white", "zinc.900")(props),
     border: "1px",
     borderColor: mode("zinc.200", "zinc.800")(props),
-    rounded: '2xl',
+    rounded: "2xl",
   },
   header: {
     p: 4,
@@ -21,30 +21,7 @@ const baseStyle = helper.definePartsStyle((props) => ({
     p: 4,
   },
 }));
-
-const userMenu = helper.definePartsStyle((props) => ({
-  header: {
-    fontSize: "medium",
-    fontWeight: "normal",
-    borderBottom: "1px",
-    borderColor: mode("zinc.200", "zinc.800")(props),
-  },
-  body: {
-    p: 0,
-  },
-  footer: {
-    bg: mode("zinc.50", "zinc.900")(props),
-    borderTop: "1px",
-    borderColor: mode("zinc.200", "zinc.800")(props),
-  },
-}));
-
-const editMenu = helper.definePartsStyle({});
 
 export const modalTheme = helper.defineMultiStyleConfig({
   baseStyle,
-  variants: {
-    userMenu,
-    editMenu,
-  },
 });
