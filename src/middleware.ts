4147e32cf4cf7aa9c697a9b48730f33313f8fba9
Chris Jardine <chris@keplux.com>
2024-08-29 23:08:36 -0400

    Fix wildcard matchers.

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/materials/(.*)",
-    "/products/(.*)",
-    "/blueprints/(.*)",
-    "/production/(.*)",
-    "/history/(.*)",
-    "/settings/(.*)",
+    "/materials(.*)",
+    "/products(.*)",
+    "/blueprints(.*)",
+    "/production(.*)",
+    "/history(.*)",
+    "/settings(.*)",
   ],
 };
