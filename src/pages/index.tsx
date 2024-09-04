4cb5feec59719151cedc332ac04d8746eda48824
Chris Jardine <chris@keplux.com>
2024-09-04 16:32:09 -0400

    CM-5 Remove unnecessary code.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,4 @@
-import { Link } from "@chakra-ui/react";
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-
-import { api } from "~/utils/api";
 
 export default function Home() {
   return (
@@ -15,36 +11,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <AuthShowcase />
-      </main>
-    </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: session } = useSession();
-
-  const { data: secretMessage } = api.material.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: session?.user !== undefined }
-  );
-
-  return (
-    <>
-      <div>
-        <p>
-          {session && <span>Logged in as {session.user?.name}</span>}
-          {secretMessage && <span> - {secretMessage}</span>}
-        </p>
-
-        <Link href="https://www.google.com" isExternal>
-          Click here
-        </Link>
-        <button onClick={session ? () => void signOut() : () => void signIn()}>
-          {session ? "Sign out" : "Sign in"}
-        </button>
-      </div>
+      <main></main>
     </>
   );
 }
