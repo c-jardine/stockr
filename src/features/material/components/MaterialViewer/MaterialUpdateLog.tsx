66c69ba9579377a3e444a5a014478fb492a13568
Chris Jardine <chris@keplux.com>
2024-09-05 15:35:11 -0400

    CM-1 Adjust layout.

--- src/features/material/components/MaterialViewer/MaterialUpdateLog.tsx
+++ src/features/material/components/MaterialViewer/MaterialUpdateLog.tsx
@@ -3,7 +3,6 @@ import {
   Circle,
   Heading,
   HStack,
-  Icon,
   Stack,
   Tag,
   Text,
@@ -11,7 +10,6 @@ import {
 } from "@chakra-ui/react";
 import { type User } from "@prisma/client";
 import React from "react";
-import { FaFile } from "react-icons/fa6";
 
 interface MaterialUpdateLogProps {
   title: React.ReactNode;
@@ -30,7 +28,7 @@ export default function MaterialUpdateLog({
   const borderColor = useColorModeValue("zinc.200", "zinc.800");
 
   return (
-    <HStack
+    <Stack
       position="relative"
       pl={4}
       py={4}
@@ -38,17 +36,19 @@ export default function MaterialUpdateLog({
       borderColor="zinc.200"
     >
       <Circle
+        position="absolute"
         bg="zinc.400"
         size={2}
-        ml="-21.5px"
+        top="22.5px"
+        left="-5px"
         outline="2px solid"
         outlineColor="white"
       />
-      <Tag fontSize="2xs" size="sm">
+      <Tag fontSize="2xs" size="sm" w="fit-content">
         {timestamp}
       </Tag>
-      <Avatar src={createdBy.image ?? undefined} size="xs" />
-      <Stack spacing={2}>
+      <HStack>
+        <Avatar src={createdBy.image ?? undefined} size="xs" />
         <Heading as="h3" fontSize="sm">
           {title}
           <Text as="span" fontWeight="normal">
@@ -57,20 +57,20 @@ export default function MaterialUpdateLog({
           </Text>
           {createdBy.name ?? "Unknown"}
         </Heading>
-        {description && (
-          <Text
-            p={2}
-            rounded="lg"
-            border="1px solid"
-            borderColor={borderColor}
-            bg={bgColor}
-            fontSize="xs"
-            textAlign="left"
-          >
-            {description}
-          </Text>
-        )}
-      </Stack>
-    </HStack>
+      </HStack>
+      {description && (
+        <Text
+          p={2}
+          rounded="lg"
+          border="1px solid"
+          borderColor={borderColor}
+          bg={bgColor}
+          fontSize="xs"
+          textAlign="left"
+        >
+          {description}
+        </Text>
+      )}
+    </Stack>
   );
 }
